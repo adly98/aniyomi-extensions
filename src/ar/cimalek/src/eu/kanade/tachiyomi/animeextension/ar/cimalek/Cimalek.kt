8268c39ed0d520fa9d0b88d84f387153d6601a5f
adly98 <medofared5@gmail.com>
2024-05-09 04:26:21 +0300

    import filter

--- src/ar/cimalek/src/eu/kanade/tachiyomi/animeextension/ar/cimalek/Cimalek.kt
+++ src/ar/cimalek/src/eu/kanade/tachiyomi/animeextension/ar/cimalek/Cimalek.kt
@@ -5,6 +5,7 @@ import android.content.SharedPreferences
 import androidx.preference.ListPreference
 import androidx.preference.PreferenceScreen
 import eu.kanade.tachiyomi.animesource.ConfigurableAnimeSource
+import eu.kanade.tachiyomi.animesource.model.AnimeFilter
 import eu.kanade.tachiyomi.animesource.model.AnimeFilterList
 import eu.kanade.tachiyomi.animesource.model.SAnime
 import eu.kanade.tachiyomi.animesource.model.SEpisode
