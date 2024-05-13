ee575b9721c77472bf21ec7a7b649e07d508bf83
adly98 <medofared5@gmail.com>
2024-05-14 02:09:27 +0300

    fix 4

--- src/ar/cimalek/src/eu/kanade/tachiyomi/animeextension/ar/cimalek/Cimalek.kt
+++ src/ar/cimalek/src/eu/kanade/tachiyomi/animeextension/ar/cimalek/Cimalek.kt
@@ -141,12 +141,21 @@ class Cimalek : ConfigurableAnimeSource, ParsedAnimeHttpSource() {
         val referer = headers.newBuilder().add("Referer", "$baseUrl/").build()
         val videoRegex = Regex("""m3u8""")
         val webViewInterceptor = client.newBuilder().addInterceptor(GetSourcesInterceptor(videoRegex)).build()
-        val lol = webViewInterceptor.newCall(GET(embedUrl, referer)).execute()
-        if (videoRegex.containsMatchIn(lol.request.url.toString())) {
-            lol.body.string().substringAfter("#EXT-X-STREAM-INF:").split("#EXT-X-STREAM-INF:").forEach {
-                val quality = it.substringAfter("RESOLUTION=").substringBefore("\n").substringAfter("x").substringBefore(",") + "p"
-                val playUrl = it.substringAfter("\n").substringBefore("\n").replace("https", "http")
-                videoList.add(Video(playUrl, "${element.text()}: $quality", playUrl, headers = referer))
+        val videoResponse = webViewInterceptor.newCall(GET(embedUrl, referer)).execute()
+        val trueVideoUrl = videoResponse.request.url.toString()
+        if (videoRegex.containsMatchIn(trueVideoUrl)) {
+            when {
+                "index-v1-a1.m3u8" in trueVideoUrl || "list.m3u8" in trueVideoUrl -> {
+                    videoList.add(Video(trueVideoUrl, element.text(), trueVideoUrl, headers = referer))
+                }
+                "master.m3u8" in trueVideoUrl -> {
+                    videoResponse.body.string().substringAfter("#EXT-X-STREAM-INF:").split("#EXT-X-STREAM-INF:").forEach {
+                        val quality = it.substringAfter("RESOLUTION=").substringBefore("\n").substringAfter("x").substringBefore(",") + "p"
+                        val playUrl = it.substringAfter("\n").substringBefore("\n")
+                        val url = if (playUrl.startsWith("index")) trueVideoUrl.replace("master","index-v1-a1") else playUrl
+                        videoList.add(Video(url, "${element.text()}: $quality", url, headers = referer))
+                    }
+                }
             }
         }
         return videoList
