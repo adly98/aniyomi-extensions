7574c4142296580f02016f51e7ff38d82df0bf6e
adly98 <medofared5@gmail.com>
2024-05-29 02:29:56 +0300

    Attempt 7

--- src/ar/test/src/eu/kanade/tachiyomi/animeextension/ar/test/Test.kt
+++ src/ar/test/src/eu/kanade/tachiyomi/animeextension/ar/test/Test.kt
@@ -60,7 +60,7 @@ class Test: ConfigurableAnimeSource, ParsedAnimeHttpSource() {
 
     override fun videoListRequest(episode: SEpisode): Request {
         val refererHeaders = headers.newBuilder().apply {
-            add("Referer", "$baseUrl/${episode.url}".toHttpUrl().toString())
+            add("Referer", "$baseUrl/${episode.url}".toHttpUrl().newBuilder().toString())
         }.build()
 
         return GET("$baseUrl/${episode.url}/watch/", headers = refererHeaders)
