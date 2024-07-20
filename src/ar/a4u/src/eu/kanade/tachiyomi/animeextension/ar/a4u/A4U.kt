101f3b2983d79b8eac73167e8e1bc609803bc69f
adly98 <medofared5@gmail.com>
2024-07-21 01:36:49 +0300

    update a4u: add filters

--- src/ar/a4u/src/eu/kanade/tachiyomi/animeextension/ar/a4u/A4U.kt
+++ src/ar/a4u/src/eu/kanade/tachiyomi/animeextension/ar/a4u/A4U.kt
@@ -42,7 +42,6 @@ class A4U: ConfigurableAnimeSource, ParsedAnimeHttpSource() {
         Injekt.get<Application>().getSharedPreferences("source_$id", 0x0000)
     }
 
-
     override fun headersBuilder() = super.headersBuilder().add("Referer", "$baseUrl/")
 
     // ============================== Popular ===============================
@@ -140,21 +139,19 @@ class A4U: ConfigurableAnimeSource, ParsedAnimeHttpSource() {
 
     override fun searchAnimeRequest(page: Int, query: String, filters: AnimeFilterList): Request {
         return if (query.isNotBlank()) {
-            GET("$baseUrl/page/$page?s=$query", headers)
+            return GET("$baseUrl/?search_param=animes&s=$query", headers)
         } else {
             val filterList = if (filters.isEmpty()) getFilterList() else filters
-            val sectionFilter = filterList.find { it is SectionFilter } as SectionFilter
             val genreFilter = filterList.find { it is GenreFilter } as GenreFilter
+            val typeFilter = filterList.find { it is TypeFilter } as TypeFilter
+            val statusFilter = filterList.find { it is StatusFilter } as StatusFilter
             val url = baseUrl.toHttpUrl().newBuilder()
-            if (sectionFilter.state != 0) {
-                url.addPathSegment(sectionFilter.toUriPart())
-            } else if (genreFilter.state != 0) {
-                url.addPathSegment(genreFilter.toUriPart().lowercase())
-            } else {
-                throw Exception("من فضلك اختر قسم او تصنيف")
+            when {
+                genreFilter.state != 0 -> url.addPathSegment(genreFilter.toUriPart())
+                typeFilter.state != 0 -> url.addPathSegment(typeFilter.toUriPart())
+                statusFilter.state != 0 -> url.addPathSegment(statusFilter.toUriPart())
+                else -> throw Exception("من فضلك اختر فلتر")
             }
-            url.addPathSegment("page")
-            url.addPathSegment("$page")
             GET(url.toString(), headers)
         }
     }
@@ -165,31 +162,79 @@ class A4U: ConfigurableAnimeSource, ParsedAnimeHttpSource() {
 
     // =============================== Filters ===============================
     override fun getFilterList() = AnimeFilterList(
-        AnimeFilter.Header("يمكنك تصفح اقسام الموقع اذا كان البحث فارغ"),
-        SectionFilter(),
-        AnimeFilter.Separator(),
-        AnimeFilter.Header("التصنيفات تعمل اذا كان 'اقسام الموقع' على 'اختر' فقط"),
+        AnimeFilter.Header("الفلترات تعمل فقط اذا كان واحد فقط هو المختار و الباقى على [اختر]"),
         GenreFilter(),
+        TypeFilter(),
+        StatusFilter(),
     )
 
-    private class SectionFilter : PairFilter(
-        "اقسام الموقع",
+    private class TypeFilter : PairFilter(
+        "النوع",
         arrayOf(
             Pair("اختر", "none"),
+            Pair("Movie", "movie-3"),
+            Pair("ONA", "ona1"),
+            Pair("OVA", "ova1"),
+            Pair("Special", "special1"),
+            Pair("TV", "tv2")
         ),
     )
 
-    private class GenreFilter : SingleFilter(
-        "التصنيف",
+    private class StatusFilter : PairFilter(
+        "الحالة",
         arrayOf(
-            "اختر", "", "",
-        ).sortedArray(),
+            Pair("اختر", "none"),
+            Pair("لم يعرض بعد", "%d9%84%d9%85-%d9%8a%d8%b9%d8%b1%d8%b6-%d8%a8%d8%b9%d8%af"),
+            Pair("مكتمل", "complete"),
+            Pair("يعرض الان", "%d9%8a%d8%b9%d8%b1%d8%b6-%d8%a7%d9%84%d8%a7%d9%86-1"),
+        ),
     )
 
-    open class SingleFilter(displayName: String, private val vals: Array<String>) :
-    AnimeFilter.Select<String>(displayName, vals) {
-        fun toUriPart() = vals[state]
-    }
+    private class GenreFilter : PairFilter(
+        "التصنيف",
+        arrayOf(
+            Pair("اختر", "none"),
+            Pair("أطفال", "%d8%a3%d8%b7%d9%81%d8%a7%d9%84"),
+            Pair("أكشن", "%d8%a3%d9%83%d8%b4%d9%86/"),
+            Pair("إيتشي", "%d8%a5%d9%8a%d8%aa%d8%b4%d9%8a/"),
+            Pair("اثارة", "%d8%a7%d8%ab%d8%a7%d8%b1%d8%a9/"),
+            Pair("العاب", "%d8%a7%d9%84%d8%b9%d8%a7%d8%a8/"),
+            Pair("بوليسي", "%d8%a8%d9%88%d9%84%d9%8a%d8%b3%d9%8a/"),
+            Pair("تاريخي", "%d8%aa%d8%a7%d8%b1%d9%8a%d8%ae%d9%8a/"),
+            Pair("جنون", "%d8%ac%d9%86%d9%88%d9%86/"),
+            Pair("جوسي", "%d8%ac%d9%88%d8%b3%d9%8a/"),
+            Pair("حربي", "%d8%ad%d8%b1%d8%a8%d9%8a/"),
+            Pair("حريم", "%d8%ad%d8%b1%d9%8a%d9%85/"),
+            Pair("خارق للعادة", "%d8%ae%d8%a7%d8%b1%d9%82-%d9%84%d9%84%d8%b9%d8%a7%d8%af%d8%a9/"),
+            Pair("خيال علمي", "%d8%ae%d9%8a%d8%a7%d9%84-%d8%b9%d9%84%d9%85%d9%8a/"),
+            Pair("دراما", "%d8%af%d8%b1%d8%a7%d9%85%d8%a7/"),
+            Pair("رعب", "%d8%b1%d8%b9%d8%a8/"),
+            Pair("رومانسي", "%d8%b1%d9%88%d9%85%d8%a7%d9%86%d8%b3%d9%8a/"),
+            Pair("رياضي", "%d8%b1%d9%8a%d8%a7%d8%b6%d9%8a/"),
+            Pair("ساموراي", "%d8%b3%d8%a7%d9%85%d9%88%d8%b1%d8%a7%d9%8a/"),
+            Pair("سحر", "%d8%b3%d8%ad%d8%b1/"),
+            Pair("سينين", "%d8%b3%d9%8a%d9%86%d9%8a%d9%86/"),
+            Pair("شريحة من الحياة", "%d8%b4%d8%b1%d9%8a%d8%ad%d8%a9-%d9%85%d9%86-%d8%a7%d9%84%d8%ad%d9%8a%d8%a7%d8%a9/"),
+            Pair("شوجو", "%d8%b4%d9%88%d8%ac%d9%88/"),
+            Pair("شوجو اَي", "%d8%b4%d9%88%d8%ac%d9%88-%d8%a7%d9%8e%d9%8a/"),
+            Pair("شونين", "%d8%b4%d9%88%d9%86%d9%8a%d9%86/"),
+            Pair("شونين اي", "%d8%b4%d9%88%d9%86%d9%8a%d9%86-%d8%a7%d9%8a/"),
+            Pair("شياطين", "%d8%b4%d9%8a%d8%a7%d8%b7%d9%8a%d9%86/"),
+            Pair("غموض", "%d8%ba%d9%85%d9%88%d8%b6/"),
+            Pair("فضائي", "%d9%81%d8%b6%d8%a7%d8%a6%d9%8a/"),
+            Pair("فنتازيا", "%d9%81%d9%86%d8%aa%d8%a7%d8%b2%d9%8a%d8%a7/"),
+            Pair("فنون قتالية", "%d9%81%d9%86%d9%88%d9%86-%d9%82%d8%aa%d8%a7%d9%84%d9%8a%d8%a9/"),
+            Pair("قوى خارقة", "%d9%82%d9%88%d9%89-%d8%ae%d8%a7%d8%b1%d9%82%d8%a9/"),
+            Pair("كوميدي", "%d9%83%d9%88%d9%85%d9%8a%d8%af%d9%8a/"),
+            Pair("محاكاة ساخرة", "%d9%85%d8%ad%d8%a7%d9%83%d8%a7%d8%a9-%d8%b3%d8%a7%d8%ae%d8%b1%d8%a9/"),
+            Pair("مدرسي", "%d9%85%d8%af%d8%b1%d8%b3%d9%8a/"),
+            Pair("مصاصي دماء", "%d9%85%d8%b5%d8%a7%d8%b5%d9%8a-%d8%af%d9%85%d8%a7%d8%a1/"),
+            Pair("مغامرات", "%d9%85%d8%ba%d8%a7%d9%85%d8%b1%d8%a7%d8%aa/"),
+            Pair("موسيقي", "%d9%85%d9%88%d8%b3%d9%8a%d9%82%d9%8a/"),
+            Pair("ميكا", "%d9%85%d9%8a%d9%83%d8%a7/"),
+            Pair("نفسي", "%d9%86%d9%81%d8%b3%d9%8a/"),
+        ),
+    )
 
     open class PairFilter(displayName: String, private val vals: Array<Pair<String, String>>) :
         AnimeFilter.Select<String>(displayName, vals.map { it.first }.toTypedArray()) {
